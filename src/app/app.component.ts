04544ce9ea91931f327e15f9920c64970eff7d3b
szabloreki <oscar1tolowski@gmail.com>
2021-09-28 17:20:11 +0200

    rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,53 @@
 import { Component } from '@angular/core';
+import { from, Observable, of, pipe } from 'rxjs';
+import { map, } from 'rxjs/operators'
+
+function MyOf(...variables: any[]) {
+  return new Observable((subcriber) => {
+    variables.forEach(x => {
+      subcriber.next(x)
+    })
+  })
+}
+
+function MyMap(operatorFunc: Function) {
+  return function (observable: Observable<any>) {
+    return new Observable((subscriber) => {
+      return observable.subscribe(
+        (next: any) => {
+          subscriber.next(operatorFunc(next))
+        },
+      )
+    })
+  }
+}
+
+// function PipeOperator(
+//   ...functs: Function[]
+// ) {
+// }
+
+function Pipe(...functs: any[]) {
+  return function (value: any) {
+    return functs.reduce((a, b) => {
+      return b(a)
+    }, value)
+  }
+}
+
+function MyInterval(ms: number) {
+  return new Observable((subcriber) => {
+    console.log('interval');
+    const intervalRef = setInterval(() => {
+      subcriber.next(500)
+      console.log('IS NEXT');
+    }, ms)
+    return () => {
+      console.log('GWAREK');
+      clearInterval(intervalRef)
+    }
+  })
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +56,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'drive-test-checker';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+    const chainedOperators = Pipe(MyMap((x: number) => x * 2)
+      , map((x: number) => x + 2))
+
+      chainedOperators(MyInterval(500)).subscribe( (x: any) => {
+        console.log(x, 'xxxx');
+      })
+
+    // const nums = MyOf(1, 2, 3)
+    // const as$ = MyMap((x: number) => x * 2)
+    // // MyInterval(1000).subscribe(x => { console.log(x, 'n')})
+    // as$(MyInterval(1000)).subscribe(x => { console.log(x, 'n') })
+    // const squareValuesMultiple = map((val: number) => val * 2)
+    // const squareValuesMultiple$ = squareValuesMultiple(squaredNums)
+    // squareValuesMultiple$.subscribe(x => console.log(x));
+  }
+
 }
+
+
